7098cff41f54e66ace968e5edbd9c1eb49de7426
Yulioss <julian_rangel_@hotmail.com>
2026-03-21 13:23:28 -0500

    implmentacion imagenes y modificacion db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,86 +1,98 @@
 export const db = [
-    {
-        id: 1,
-        name: 'Lukather',
-        image: 'guitarra_01',
-        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
-        price: 299,
-    },
-    {
-        id: 2,
-        name: 'SRV',
-        image: 'guitarra_02',
-        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
-        price: 349,
-    },
-    {
-        id: 3,
-        name: 'Borland',
-        image: 'guitarra_03',
-        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
-        price: 329,
-    },
-    {
-        id: 4,
-        name: 'VAI',
-        image: 'guitarra_04',
-        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
-        price: 299,
-    },
-    {
-        id: 5,
-        name: 'Thompson',
-        image: 'guitarra_05',
-        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
-        price: 399,
-    },
-    {
-        id: 6,
-        name: 'White',
-        image: 'guitarra_06',
-        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
-        price: 329,
-    },
-    {
-        id: 7,
-        name: 'Cobain',
-        image: 'guitarra_07',
-        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
-        price: 349,
-    },
-    {
-        id: 8,
-        name: 'Dale',
-        image: 'guitarra_08',
-        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
-        price: 379,
-    },
-    {
-        id: 9,
-        name: 'Krieger',
-        image: 'guitarra_09',
-        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
-        price: 289,
-    },
-    {
-        id: 10,
-        name: 'Campbell',
-        image: 'guitarra_10',
-        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
-        price: 349,
-    },
-    {
-        id: 11,
-        name: 'Reed',
-        image: 'guitarra_11',
-        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
-        price: 399,
-    },
-    {
-        id: 12,
-        name: 'Hazel',
-        image: 'guitarra_12',
-        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
-        price: 379,
-    },
-  ]
+  {
+    id: 1,
+    name: 'Lukather',
+    image: 'guitarra_01',
+    description: '...',
+    price: 299,
+    category: 'electric'
+  },
+  {
+    id: 2,
+    name: 'SRV',
+    image: 'guitarra_02',
+    description: '...',
+    price: 349,
+    category: 'electric'
+  },
+  {
+    id: 3,
+    name: 'Borland',
+    image: 'guitarra_03',
+    description: '...',
+    price: 329,
+    category: 'electric'
+  },
+  {
+    id: 4,
+    name: 'VAI',
+    image: 'guitarra_04',
+    description: '...',
+    price: 299,
+    category: 'electric'
+  },
+  {
+    id: 5,
+    name: 'Thompson',
+    image: 'guitarra_05',
+    description: '...',
+    price: 399,
+    category: 'acoustic'
+  },
+  {
+    id: 6,
+    name: 'White',
+    image: 'guitarra_06',
+    description: '...',
+    price: 329,
+    category: 'acoustic'
+  },
+  {
+    id: 7,
+    name: 'Cobain',
+    image: 'guitarra_07',
+    description: '...',
+    price: 349,
+    category: 'acoustic'
+  },
+  {
+    id: 8,
+    name: 'Dale',
+    image: 'guitarra_08',
+    description: '...',
+    price: 379,
+    category: 'bass'
+  },
+  {
+    id: 9,
+    name: 'Krieger',
+    image: 'guitarra_09',
+    description: '...',
+    price: 289,
+    category: 'bass'
+  },
+  {
+    id: 10,
+    name: 'Campbell',
+    image: 'guitarra_10',
+    description: '...',
+    price: 349,
+    category: 'bass'
+  },
+  {
+    id: 11,
+    name: 'Reed',
+    image: 'guitarra_11',
+    description: '...',
+    price: 399,
+    category: 'electric'
+  },
+  {
+    id: 12,
+    name: 'Hazel',
+    image: 'guitarra_12',
+    description: '...',
+    price: 379,
+    category: 'acoustic'
+  }
+]
