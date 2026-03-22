3f1edc84e8e4c8175cf620e7def8c6b47c933e41
Yulioss <julian_rangel_@hotmail.com>
2026-03-21 19:25:03 -0500

    ajustes varios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CartProvider } from './context/CartContext/CartContext'
-
+import { Container } from "react-bootstrap";
 import { NavBar } from './components/Navbar/Navbar'
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer'
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer'
@@ -8,14 +8,19 @@ import { CheckOut } from './components/CheckOut/CheckOut'
 
 function App() {
   return (
-    <CartProvider>
-      <BrowserRouter>
-        <NavBar />
+<CartProvider>
+  <BrowserRouter>
+
+    <div className="d-flex flex-column min-vh-100">
+
+      <NavBar />
+
+      <main className="flex-grow-1">
         <Routes>          
           <Route
             path="/"
             element={
-              <ItemListContainer greeting="Bienvenido a la tienda" />
+              <ItemListContainer greeting="Bienvenido a el mundo de la guitarra" />
             }
           />
           <Route
@@ -33,9 +38,20 @@ function App() {
             element={<CheckOut />}
           />
         </Routes>
+      </main>
 
-      </BrowserRouter>
-    </CartProvider>
+      <footer className="bg-dark py-4">
+        <Container>
+          <p className="text-white text-center m-0">
+            Copyright © 2026 un proyecto de Julian Rangel
+          </p>
+        </Container>
+      </footer>
+
+    </div>
+
+  </BrowserRouter>
+</CartProvider>
   )
 }
 
